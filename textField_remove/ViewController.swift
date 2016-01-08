683e4dec29648372746cc3fe80fc9436885f77c2
apple <apple@appletekiMacBook-Pro.local>
2016-01-08 09:39:35 +0800

    Initial Commit

---
+++ textField_remove/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textField_remove
+//
+//  Created by apple on 16/1/8.
+//  Copyright (c) 2016年 mountor_zj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
